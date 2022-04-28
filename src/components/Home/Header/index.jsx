7cfb0f6d1613d6593f0b6161041dde7359777f68
neha-02-moon <nehamoon02@gmail.com>
2022-04-28 23:43:01 +0530

    Blog

---
+++ src/components/Home/Header/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './styles.css';
+
+const Header = () => (
+  <header className='home-header'>
+    <h2>Pepper Content</h2>
+    <h3>Welcome</h3>
+    <h1>
+      <span>“</span> Blog <span>”</span>
+    </h1>
+    <p>
+      awesome place to make oneself <br /> productive and entertained through
+      daily updates.
+    </p>
+  </header>
+);
+
+export default Header;
